9ce898d285770916aef4d5d8a12753b765903512
gitapp <eazhumalai.kaliyaperumal@pwc.com>
2023-04-14 17:15:55 +0100

    added rest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+     .then((response) => response.json())
+     .then((actualData) => {
+      setData(actualData);
+    });
+   }, []);
+
   return (
     <div className="App">
       <h1>Welcome to React App fourth commit</h1>
+      <ol>
+        {data &&
+          data.map(({ id, title }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+            </li>
+          ))}
+      </ol>
     </div>
   );
 }
