664a4586123798e1e9849095ed5d37f00ac4e1b6
eazhumalaipwc <eazhumalai.kaliyaperumal@pwc.com>
2023-04-17 16:12:33 +0100

    added restapi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { useEffect,useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Card } from '@mui/material';
 import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    fetch(`https://testapp7184-sandbox.mxapps.io/rest/myservicecases/v1/GetAllCase`)
      .then((response) => response.json())
      .then((actualData) => {
       setData(actualData);
+      console.log(actualData);
     });
    }, []);
 
   return (
     <div className="App">
-      <h1>Welcome to React App fourth commit</h1>
-      <ol>
+      <h1>Welcome to React App </h1>
+      <ol class="list-group">
         {data &&
-          data.map(({ id, title }) => (
-            <li key={id}>
-              <h3>{title}</h3>
+          data.map(({ id, CaseId, CaseName }) => (
+            <li  key={id}>
+              <h3 class="list-group-item">{CaseId +'  '+CaseName}</h3>
             </li>
           ))}
       </ol>
